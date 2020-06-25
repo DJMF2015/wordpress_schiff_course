551ef339f45d29980cee692fe0ae8a90553a48de
DJMF2015 <djm.fulton@gmail.com>
2020-06-25 16:05:02 +0100

    added wp function to handle dynamic child-parent pages for about us content

--- wp-content/themes/university_theme/page.php
+++ wp-content/themes/university_theme/page.php
@@ -5,37 +5,48 @@ while(have_posts()) {
   the_post(); ?>
 
   <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php the_title(); ?></h1>
-    <div class="page-banner__intro">
-      <p>DONT FORGET TO REPLACE ME LATER</p>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+      <div class="page-banner__intro">
+        <p>DONT FORGET TO REPLACE ME LATER</p>
+      </div>
     </div>
   </div>
-</div>
 
   <div class="container container--narrow page-section">
 
+    <?php
+    if (wp_get_post_parent_id(get_the_ID())) { ?>
+      <!-- //return parent page if one exists. Therefore the below function is able to retrieve the child page of the paret page if one exists and will run if the current page is a child page -->
+      <!-- only wish to view below meta side box div if the current is a child page -->
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"> </<?php the_title(); ?> </span></p>
+      </div>
+    <?php }
+    ?>
+
+
+    <!-- only wish to view below meta side box div if the current is a child page -->
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"> </<?php the_title(); ?></span></p>
     </div>
 
     <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div> -->
+    <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <ul class="min-list">
+    <li class="current_page_item"><a href="#">Our History</a></li>
+    <li><a href="#">Our Goals</a></li>
+  </ul>
+</div> -->
 
-    <div class="generic-content">
-      <?php the_content(); ?>
-      <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p> -->
-    </div>
-    </div>
-  <?php }
+<div class="generic-content">
+  <?php the_content(); ?>
+  <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
 
-  get_footer();
+</div>
+<?php }
 
-  ?>
+get_footer();
+
+?>

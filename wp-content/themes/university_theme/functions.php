6c7429eb3384a821aabacd4cd8e0393719afcf39
DJMF2015 <djm.fulton@gmail.com>
2020-06-26 13:30:08 +0100

    added cookie policy page

--- wp-content/themes/university_theme/functions.php
+++ wp-content/themes/university_theme/functions.php
@@ -8,3 +8,9 @@ function university_files() {
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features(){
+  // to enable a featrue for your theme...
+  add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'university_features');
